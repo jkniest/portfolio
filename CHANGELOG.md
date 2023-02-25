4c8a43468515e9fe57728b11f3a3b12fecb1c04d
Jordan Kniest <j.kniest@basecom.de>
2023-02-25 18:40:26 +0100

    Add google fonts package (#5)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,3 +15,4 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Added `@nuxt/content` (v2.4.3)
 - Added `sitemap` (v7.1.1)
 - Added `nuxt-link-checker` (v1.0.3)
+- Added `@nuxtjs/google-fonts` (v3.0.0-1)
--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,14 @@
 
     <NuxtLink :to="{path: '/blog/shopware6/create-offer-plugin/01-preparations'}">Example Blog Post</NuxtLink>
 </template>
+
+<style>
+h1 {
+    font-family: 'Barlow Condensed', sans-serif;
+    font-weight: bold;
+}
+
+body {
+    font-family: 'Montserrat', sans-serif;
+}
+</style>
