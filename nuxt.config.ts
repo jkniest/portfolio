b1f18c18f969be4d51ee1dacb4d6ac664e77d31c
Jordan Kniest <mail@jkniest.de>
2023-02-25 19:23:41 +0100

    Add color mode module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,7 @@ export default defineNuxtConfig({
         '@nuxt/content',
         'nuxt-link-checker',
         '@nuxtjs/tailwindcss',
+        '@nuxtjs/color-mode',
         ['@nuxtjs/google-fonts', {
             families: {
                 'Barlow Condensed': [600],
@@ -32,5 +33,8 @@ export default defineNuxtConfig({
                 dark: 'github-dark'
             },
         }
+    },
+    colorMode: {
+        classSuffix: ''
     }
 })
