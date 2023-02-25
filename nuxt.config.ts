fb01284b13d962430dd5175661c24e032a3353dc
Jordan Kniest <mail@jkniest.de>
2023-02-25 17:52:18 +0100

    Use vercel url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
     },
     runtimeConfig: {
         public: {
-            host: 'http://localhost:3000'
+            host: process.env.NUXT_ENV_VERCEL_URL || 'http://localhost:3000'
         }
     },
     content: {
