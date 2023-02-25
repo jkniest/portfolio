aeb5873839506e16e10e36639d100d58150a9d56
Jordan Kniest <j.kniest@basecom.de>
2023-02-25 17:17:53 +0100

    Add nuxt content module, catch all route for blog and sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: [
+        '@nuxt/content'
+    ],
     typescript: {
         strict: true
+    },
+    nitro: {
+        prerender: {
+            routes: ['/sitemap.xml']
+        }
+    },
+    runtimeConfig: {
+        public: {
+            host: 'http://localhost:3000'
+        }
     }
 })
