6ec5665c1659fb24c739bef23b593210ad4cf2d9
Jordan Kniest <mail@jkniest.de>
2023-04-22 22:53:45 +0200

    Add vue auto animate and register it

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+const open = ref(false);
+</script>
+
 <template>
   <main class="p-10 h-screen w-screen dark:bg-gray-800 bg-gray-100">
     <h1 class="text-3xl">
@@ -11,6 +15,17 @@
     <NuxtLink :to="{ path: '/speaking' }" class="text-blue-600 block mt-3">
       Speaking
     </NuxtLink>
+
+    <div v-auto-animate>
+      <button @click="open = !open">
+        Toggle
+      </button>
+
+      <div v-if="open">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Id repellat harum mollitia fuga molestias earum, vel
+        impedit fugit non error magni officia ab illum veniam! Beatae alias rem pariatur facere!
+      </div>
+    </div>
 
     <select v-model="$colorMode.preference">
       <option value="system">
@@ -31,12 +46,12 @@
 
 <style lang="postcss">
 h1 {
-    @apply font-heading;
-    font-weight: bold;
-    view-transition-name: title;
+  @apply font-heading;
+  font-weight: bold;
+  view-transition-name: title;
 }
 
 body {
-    @apply font-content;
+  @apply font-content;
 }
 </style>
