6cf23d9ab3ce4f9dc755b0cb3cea6932d9653468
Jordan Kniest <mail@jkniest.de>
2023-05-03 08:43:34 +0200

    Linting

--- components/Animation/ParticleCanvas.vue
+++ components/Animation/ParticleCanvas.vue
@@ -13,10 +13,10 @@ const props = withDefaults(defineProps<{
     trailLength?: number;
     particleCount?: number;
 }>(), {
-    radius: 5,
-    maxSpeed: 3.5,
-    trailLength: 30,
-    particleCount: 70,
+  radius: 5,
+  maxSpeed: 3.5,
+  trailLength: 30,
+  particleCount: 70
 });
 
 const canvas = ref<HTMLCanvasElement>();
@@ -25,132 +25,130 @@ const height = ref<number>(0);
 const particles = ref<Particle[]>([]);
 
 onMounted(() => {
-    width.value = window.innerWidth;
-    height.value = window.innerHeight;
+  width.value = window.innerWidth;
+  height.value = window.innerHeight;
 
-    for (let i = 0; i < props.particleCount; i++) {
-        particles.value.push({
-            x: Math.random() * width.value,
-            y: Math.random() * height.value,
-            dx: (Math.random() * props.maxSpeed - 1) * props.maxSpeed,
-            dy: (Math.random() * props.maxSpeed - 1) * props.maxSpeed,
-            trail: []
-        });
-    }
+  for (let i = 0; i < props.particleCount; i++) {
+    particles.value.push({
+      x: Math.random() * width.value,
+      y: Math.random() * height.value,
+      dx: (Math.random() * props.maxSpeed - 1) * props.maxSpeed,
+      dy: (Math.random() * props.maxSpeed - 1) * props.maxSpeed,
+      trail: []
+    });
+  }
 
-    if(canvas.value) {
-        canvas.value.width = width.value;
-        canvas.value.height = height.value;
-    }
-    
-    animationLoop();
+  if (canvas.value) {
+    canvas.value.width = width.value;
+    canvas.value.height = height.value;
+  }
+
+  animationLoop();
 });
 
-function capVelocity(particle: Particle): void {
-    const speed = Math.sqrt(particle.dx * particle.dx + particle.dy * particle.dy);
+function capVelocity (particle: Particle): void {
+  const speed = Math.sqrt(particle.dx * particle.dx + particle.dy * particle.dy);
 
-    if (speed > props.maxSpeed) {
-        const ratio = props.maxSpeed / speed;
-        particle.dx *= ratio;
-        particle.dy *= ratio;
+  if (speed > props.maxSpeed) {
+    const ratio = props.maxSpeed / speed;
+    particle.dx *= ratio;
+    particle.dy *= ratio;
+  }
+}
+
+function areColliding (p1: Particle, p2: Particle): boolean {
+  const distance = Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
+  return distance < 2 * props.radius;
+}
+
+function reflect (p1: Particle, p2: Particle): void {
+  const dx = p2.x - p1.x;
+  const dy = p2.y - p1.y;
+  const distance = Math.sqrt(dx * dx + dy * dy);
+
+  if (distance >= 2 * props.radius) { return; } // No collision
+
+  const normalX = dx / distance;
+  const normalY = dy / distance;
+
+  const relativeVelocityX = p1.dx - p2.dx;
+  const relativeVelocityY = p1.dy - p2.dy;
+
+  const dot = relativeVelocityX * normalX + relativeVelocityY * normalY;
+
+  if (dot < 0) { return; } // Particles are moving away from each other, so no need to reflect
+
+  const impulse = 2 * dot;
+
+  p1.dx -= impulse * normalX;
+  p1.dy -= impulse * normalY;
+  p2.dx += impulse * normalX;
+  p2.dy += impulse * normalY;
+
+  capVelocity(p1);
+  capVelocity(p2);
+}
+
+function drawParticles () {
+  const ctx = canvas.value?.getContext('2d');
+  if (!ctx) {
+    return;
+  }
+
+  ctx.clearRect(0, 0, width.value, height.value);
+  particles.value.forEach((particle) => {
+    for (let i = 0; i < particle.trail.length - 1; i++) {
+      const progress = 1 - (i / (particle.trail.length - 1));
+      const opacity = 0.5 - progress;
+
+      ctx.beginPath();
+      ctx.strokeStyle = `rgba(251, 191, 36, ${opacity})`;
+      ctx.lineWidth = 1.3;
+      ctx.moveTo(particle.trail[i].x, particle.trail[i].y);
+      ctx.lineTo(particle.trail[i + 1].x, particle.trail[i + 1].y);
+      ctx.stroke();
     }
+  });
 }
 
-function areColliding(p1: Particle, p2: Particle): boolean {
-    const distance = Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
-    return distance < 2 * props.radius;
-}
-
-function reflect(p1: Particle, p2: Particle): void {
-    const dx = p2.x - p1.x;
-    const dy = p2.y - p1.y;
-    const distance = Math.sqrt(dx * dx + dy * dy);
-
-    if (distance >= 2 * props.radius) return; // No collision
-
-    const normalX = dx / distance;
-    const normalY = dy / distance;
-
-    const relativeVelocityX = p1.dx - p2.dx;
-    const relativeVelocityY = p1.dy - p2.dy;
-
-    const dot = relativeVelocityX * normalX + relativeVelocityY * normalY;
-
-    if (dot < 0) return; // Particles are moving away from each other, so no need to reflect
-
-    const impulse = 2 * dot;
-
-    p1.dx -= impulse * normalX;
-    p1.dy -= impulse * normalY;
-    p2.dx += impulse * normalX;
-    p2.dy += impulse * normalY;
-
-    capVelocity(p1);
-    capVelocity(p2);
-}
-
-function drawParticles() {
-    const ctx = canvas.value?.getContext('2d');
-    if (!ctx) {
-        return;
+function updateParticles () {
+  particles.value.forEach((particle) => {
+    particle.trail.push({ x: particle.x, y: particle.y });
+    if (particle.trail.length > props.trailLength) {
+      particle.trail.shift();
     }
 
-    ctx.clearRect(0, 0, width.value, height.value);
-    particles.value.forEach((particle) => {
-        for (let i = 0; i < particle.trail.length - 1; i++) {
-            const progress = 1 - (i / (particle.trail.length - 1));
-            const opacity = .5 - progress;
+    particle.x += particle.dx;
+    particle.y += particle.dy;
 
-            ctx.beginPath();
-            ctx.strokeStyle = `rgba(251, 191, 36, ${opacity})`;
-            ctx.lineWidth = 1.3;
-            ctx.moveTo(particle.trail[i].x, particle.trail[i].y);
-            ctx.lineTo(particle.trail[i + 1].x, particle.trail[i + 1].y);
-            ctx.stroke();
-        }
-    });
-}
-
-function updateParticles() {
-    particles.value.forEach((particle) => {
-        particle.trail.push({ x: particle.x, y: particle.y });
-        if (particle.trail.length > props.trailLength) {
-            particle.trail.shift();
-        }
-
-        particle.x += particle.dx;
-        particle.y += particle.dy;
-
-        if (particle.x < 0 || particle.x > width.value) {
-            particle.dx *= -1;
-        }
-
-        if (particle.y < 0 || particle.y > height.value) {
-            particle.dy *= -1;
-        }
-    });
-}
-
-function checkCollisions() {
-    for (let i = 0; i < particles.value.length; i++) {
-        for (let j = i + 1; j < particles.value.length; j++) {
-            if (areColliding(particles.value[i], particles.value[j])) {
-                reflect(particles.value[i], particles.value[j]);
-            }
-        }
+    if (particle.x < 0 || particle.x > width.value) {
+      particle.dx *= -1;
     }
+
+    if (particle.y < 0 || particle.y > height.value) {
+      particle.dy *= -1;
+    }
+  });
 }
 
-function animationLoop() {
-    updateParticles();
-    checkCollisions();
-    drawParticles();
-    requestAnimationFrame(animationLoop);
+function checkCollisions () {
+  for (let i = 0; i < particles.value.length; i++) {
+    for (let j = i + 1; j < particles.value.length; j++) {
+      if (areColliding(particles.value[i], particles.value[j])) {
+        reflect(particles.value[i], particles.value[j]);
+      }
+    }
+  }
+}
+
+function animationLoop () {
+  updateParticles();
+  checkCollisions();
+  drawParticles();
+  requestAnimationFrame(animationLoop);
 }
 </script>
 
 <template>
-    <canvas class="fixed inset-0 pointer-events-none" ref="canvas">
-
-    </canvas>
+  <canvas ref="canvas" class="fixed inset-0 pointer-events-none" />
 </template>
--- components/Homepage/Introduction.vue
+++ components/Homepage/Introduction.vue
@@ -7,37 +7,43 @@ const openWork = () => emits('open-work');
 </script>
 
 <template>
-    <div class="p-12">
-        <h2 class="text-4xl font-heading">Ahoy!</h2>
+  <div class="p-12">
+    <h2 class="text-4xl font-heading">
+      Ahoy!
+    </h2>
 
-        <div class="flex mt-4">
-            <p class="text-lg">
-                Hello! I'm Jordan, a software developer from Germany. My interest in computers and programming began when I
-                was just 8 years old. I started by learning the basics of HTML and CSS, and soon found myself delving into
-                more complex languages such as Javascript and PHP.
-            </p>
+    <div class="flex mt-4">
+      <p class="text-lg">
+        Hello! I'm Jordan, a software developer from Germany. My interest in computers and programming began when I
+        was just 8 years old. I started by learning the basics of HTML and CSS, and soon found myself delving into
+        more complex languages such as Javascript and PHP.
+      </p>
 
-            <div class="ml-3">
-                <AnimationPulsatingCircle :size="150" :inner-size="140">
-                    <img src="https://placehold.it/140x140" alt="Jordan Kniest" class="rounded-full inner-circle">
-                </AnimationPulsatingCircle>
-            </div>
-        </div>
-
-        <p class="text-lg mt-8">
-            Fast forward to today, and I'm grateful to have turned my hobby into a fulfilling career. I currently hold the
-            position of Head of Development at basecom, where I continue to learn and grow in my professional journey. <a
-                href="#" @click.prevent="openWork" class="text-sky-600 border-b border-sky-600">Learn more</a>
-        </p>
-
-        <p class="text-lg mt-8">
-            In my spare time, I love unwinding with video games, diving into board games with friends, participating in
-            open-source projects, and occasionally penning a blog post. When I'm not immersed in these activities, you can
-            often catch me enjoying cocktails or a cold beer with my buddies.
-        </p>
-
-        <div class="text-right">
-            <a href="https://twitter.com/@j_kniest" class="text-sky-600 border-b border-sky-600" target="_blank">Follow me
-                on Twitter</a>
+      <div class="ml-3">
+        <AnimationPulsatingCircle :size="150" :inner-size="140">
+          <img src="https://placehold.it/140x140" alt="Jordan Kniest" class="rounded-full inner-circle">
+        </AnimationPulsatingCircle>
+      </div>
     </div>
-</div></template>
+
+    <p class="text-lg mt-8">
+      Fast forward to today, and I'm grateful to have turned my hobby into a fulfilling career. I currently hold the
+      position of Head of Development at basecom, where I continue to learn and grow in my professional journey. <a
+        href="#"
+        class="text-sky-600 border-b border-sky-600"
+        @click.prevent="openWork"
+      >Learn more</a>
+    </p>
+
+    <p class="text-lg mt-8">
+      In my spare time, I love unwinding with video games, diving into board games with friends, participating in
+      open-source projects, and occasionally penning a blog post. When I'm not immersed in these activities, you can
+      often catch me enjoying cocktails or a cold beer with my buddies.
+    </p>
+
+    <div class="text-right">
+      <a href="https://twitter.com/@j_kniest" class="text-sky-600 border-b border-sky-600" target="_blank">Follow me
+        on Twitter</a>
+    </div>
+  </div>
+</template>
